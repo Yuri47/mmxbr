d3d0cd95abb82bafa5d6e616497c31dc902c34b7
Yuri47 <yuri.alexs@gmail.com>
2016-11-22 20:12:51 -0200

    testing with modal

---
+++ resources/views/teste.php
@@ -0,0 +1,241 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>jQuery iziModal Plugin Demos</title>
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+<link href="css/iziModal.min.css" rel="stylesheet" type="text/css">
+</head>
+
+
+
+
+
+
+<body>
+<div class="container" style="margin-top:50px;">
+<h1 class="center-block">jQuery iziModal Plugin Demos</h1>
+ 
+ 
+ </div>
+
+ 
+
+<!-- Large Modal -->
+<a class="waves-effect waves-light btn-large red darken-2 trigger-largeContent">Large content</a>
+<div id="modal-Large">
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore. Dolorum perferendis, non nisi quisquam animi iusto, officiis eligendi eos, fugit, dolore corporis provident accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ea ad aspernatur, temporibus iusto cum autem repudiandae, delectus non voluptas corporis facere cumque! Qui accusantium ratione eum eligendi maiores velit ea porro voluptatibus architecto dolore.<p>
+</div>
+<!-- End Large Modal -->
+
+<br>
+
+<!-- Large Modal with gallery -->
+<!-- Large content with gallery, using iSwiper - http://idangero.us/swiper/get-started/  -->
+<a href="#" class="waves-effect waves-light btn-large red darken-2 trigger-large-gallery">Large content with gallery</a>
+ 
+<!-- Swiper -->
+<div id="modal-Large-gallery">
+<div class="swiper-container">
+  <div class="swiper-wrapper">
+    <div class="swiper-slide"><img src="http://ultraimg.com/images/2016/07/22/orvalho.jpg" alt="orvalho.jpg" border="0" /></div>
+    <div class="swiper-slide"><img src="http://ultraimg.com/images/2016/07/22/tree.jpg" alt="tree.jpg" border="0" /></div>
+    <div class="swiper-slide"><img src="http://ultraimg.com/images/2016/07/22/field.jpg" alt="field.jpg" border="0" /></div>
+  </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+</div>
+  
+</div>
+<!-- End large Modal with gallery -->
+
+
+
+
+<style type="text/css">
+    @import url(http://fonts.googleapis.com/css?family=Lato:400);
+
+/* Some reset */
+@charset "utf-8";
+html, body, div, span, applet, object, iframe, h1, h2, h3,
+h4, h5, h6, p, blockquote, pre, a, abbr, acronym,
+address, big, cite, code, del, dfn, em, font, img, ins,
+kbd, q, s, samp, small, strike, strong, sub, sup, tt,
+var, dl, dt, dd, ol, ul, li, fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    font-weight: inherit;
+    font-style: inherit;
+    font-size: 100%;
+    font-family: inherit;
+    vertical-align: baseline;
+}
+:focus { outline: 0;}
+a:link, a:visited, a:active{text-decoration:none; color: #21b3b4}
+table { border-collapse: separate;border-spacing: 0; width: 100%;}
+th, td {text-align: left; font-weight: normal;}
+tr{line-height: 30px;}
+img, iframe {border: none; text-decoration:none;}
+ol, ul {list-style: none;}
+input, textarea, select, button {font-size: 100%;font-family: inherit;}
+*, *:after, *::before{box-sizing:border-box; -ms-box-sizing:border-box; -moz-box-sizing:border-box; -webkit-box-sizing:border-box;}
+
+
+/* Main */
+
+body {
+  background: #444B54;
+  padding: 15px;
+}
+
+.trigger-largeContent,
+.trigger-large-gallery {
+  font-family: Lato;
+  font-size: 20px;
+  color: white;
+  width: 300px;
+  padding: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  overflow: hidden;
+}
+
+p {
+  padding: 15px;
+  font-family: Lato;
+  font-size: 20px;
+  text-align: justify;
+}
+
+.iziModal .iziModal-header-title , .iziModal-header-subtitle {
+    font-family: Lato;
+    font-weight: 100;
+    font-size: 25px;
+}
+
+/* Swiper */
+
+.swiper-slide img {
+  width: 100%;
+}
+
+.swiper-container {
+  width: 100%;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.0/js/swiper.min.js"></script>
+<script src="js/iziModal.min.js"></script>
+
+ 
+
+<script type="text/javascript">
+
+ // Large content
+$(document).on('click', '.trigger-largeContent', function (event) {
+    event.preventDefault();
+    $('#modal-Large').iziModal('open', this); // Do not forget the "this"
+}); 
+$("#modal-Large").iziModal({
+    title: 'A random title',
+    subtitle: 'For a random text',
+    theme: '',
+    headerColor: '#e65100',
+    overlayColor: 'rgba(0, 0, 0, 0.4)',
+    iconColor: '',
+    iconClass: null,
+    width: 800,
+    padding: 0,
+    overlayClose: true,
+    closeOnEscape: true,
+    bodyOverflow: false,
+});
+
+// Large with gallery
+$(document).on('click', '.trigger-large-gallery', function (event) {
+    event.preventDefault();
+    $('#modal-Large-gallery').iziModal('open', this); // Do not forget the "this"
+}); 
+$("#modal-Large-gallery").iziModal({
+    title: 'A random title',
+    subtitle: 'For a random gallery',
+    theme: '',
+    headerColor: '#e65100 ',
+    overlayColor: 'rgba(0, 0, 0, 0.4)',
+    iconColor: '',
+    iconClass: null,
+    fullscreen: true,
+    width: 600,
+    padding: 0,
+    overlayClose: true,
+    closeOnEscape: true,
+    bodyOverflow: false,
+    focusInput: true,
+    onFullscreen: function(modal){
+        console.log(modal.isFullscreen);
+    },
+    onOpened: function() {
+        var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        loop: true
+    });
+  },
+});
+
+
+</script>
+
+
+
+
+
+</body>
+</html>
